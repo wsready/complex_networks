45ec336403d39ca5b685645d5b965350e43f195d
wangsong <384097785@qq.com>
2023-06-08 14:47:03 +0800

    Initial commit

--- final_task/final_task.py
+++ final_task/final_task.py
@@ -1,18 +1,7 @@
 # @Time    : 2023/6/4 15:36
 # @Author  : wang song
 # @File    : final_task.py
-# @Description : 解题思路如下
-# 1. N=1000个节点组成的BA无标度网络可以用networkX库中的nx.barabasi_albert_graph(N, m)'函数来生成。
-# 其中每个节点代表一个决策个体，并且具有连接的节点可以进行博弈决策。每个个体可以选择两种决策行为：A和B。
-# 2. 在每一轮的博弈决策过程中，有两个阶段：博弈阶段和决策更新阶段。
-#    1. 首先在博弈阶段，每个个体与其邻居进行博弈，根据与邻居的决策行为获得效益。
-#       当A决策个体与A决策个体博弈时，效益为3。
-#       当A决策个体与B决策个体博弈时，效益为0。
-#       当B决策个体与A决策个体博弈时，效益为5。
-#       当B决策个体与B决策个体博弈时，效益为1。
-#    2. 在博弈阶段结束后，每个个体需要更新自己的决策行为。
-#    3. 更新决策行为时，个体i会随机选择一个邻居j进行决策学习，同时个体i将自己的决策行为调整为个体j的决策行为为概率p。p=1/(1+exp[(𝑈𝑖−𝑈𝑗)/0.5])，其中𝑈𝑖和𝑈𝑗分别代表个体i和个体j的累加效益。
-# 3. 最后再用matplotlib库画出来群体中选择A决策行为个体的比例在10000轮博弈决策过程中随时间的变化曲线。
+# @Description : 期末考核代码实现
 import networkx as nx
 import random
 import numpy as np
@@ -39,7 +28,7 @@ def play_game(G, decisions):
     """
     N = len(decisions)
     utilities = np.zeros(N)
-
+    # 定义决策博弈的规则
     for i in range(N):
         for neighbor in G.neighbors(i):
             if decisions[i] == 'A' and decisions[neighbor] == 'A':
@@ -65,11 +54,14 @@ def update_decisions(G, decisions, utilities):
     N = len(decisions)
 
     for i in range(N):
+        # 随机选择一个邻居进行决策学习
         neighbor = random.choice(list(G.neighbors(i)))
         utility_diff = utilities[i] - utilities[neighbor]
+        # 考核内容所给概率
         p = 1 / (1 + np.exp(utility_diff / 0.5))
 
-        if random.random() < p:
+        # 设置一个随机数与p比较，满足条件则更新自己的决策行为
+        if random.random() >= p:
             decisions[i] = decisions[neighbor]
 
 
@@ -81,23 +73,30 @@ def simulate_game(N, m, num_rounds):
     :param num_rounds: 仿真的轮数
     :return:
     """
+    # 创建一个BA无标度网络
     G = create_scale_free_network(N, m)
+
+    # 初始化决策行为
     decisions = np.random.choice(['A', 'B'], size=N)
+    # 存储选择A决策行为的比例随时间的变化
     proportions = []
 
+    # 进行10000轮博弈决策
     for _ in range(num_rounds):
+        # 计算选择A的概率
         count_A = np.count_nonzero(decisions == 'A')
         proportion_A = count_A / N
         proportions.append(proportion_A)
 
+        # 博弈之后得到的效益,以及更新效益
         utilities = play_game(G, decisions)
         update_decisions(G, decisions, utilities)
 
     # 绘制个体选择A决策行为的比例随时间的变化曲线
     plt.plot(proportions)
-    plt.xlabel('轮次')
-    plt.ylabel('选择 A 决策行为个体的比例')
-    # plt.title('Evolution of Decision Behavior in a Scale-Free Network')
+    plt.xlabel('Time')
+    plt.ylabel('Proportion of individuals choosing A')
+    plt.title('Evolution of Decision Behavior in a Scale-Free Network')
     plt.show()
 
 
--- readme.md
+++ readme.md
@@ -14,4 +14,4 @@
 #### 三个文件的子目录相同
 ##### .py文件是实现代码、.png文件是代码画出来的图、.md文件是作业思路及说明。
 
-##### 作业已上传至github(https://github.com/wsready/complex_networks)，也可在线查看。
+##### 作业已上传至github(https://github.com/wsready/complex_networks),也可在线查看。
